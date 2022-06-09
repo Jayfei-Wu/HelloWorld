0b1315a14b33214e1f03d00afdefcacb79b1e33c
Jayfei <1048351273@qq.com>
2022-06-09 17:24:27 +0800

    test2 commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("wjf-first");
 
     }
 }
