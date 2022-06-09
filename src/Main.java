f4559dffacece8967357fcbcbcadcdf44108348f
Jayfei <1048351273@qq.com>
2022-06-09 17:23:15 +0800

    test1 commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("conmit");
+        System.out.println("master");
 
     }
 }
