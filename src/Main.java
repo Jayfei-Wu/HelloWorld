20c3b5b4fe44e43a87e27261e7be0547b8957de5
Jayfei <1048351273@qq.com>
2022-06-09 22:21:41 +0800

    冲突测试2 commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("master3");
+        System.out.println("first");
+        System.out.println("first");
+        System.out.println("first");
+        System.out.println("first");
+        System.out.println("冲突测试2");
 
     }
 }
