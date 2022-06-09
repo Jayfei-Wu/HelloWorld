dd6a502fb515cc45b0853ec1b1a8626720605dc8
Jayfei <1048351273@qq.com>
2022-06-09 17:33:06 +0800

    test3 commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("wjf-first");
+        System.out.println("master3");
 
     }
 }
